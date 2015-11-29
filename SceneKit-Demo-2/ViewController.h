e5bd6a9fa840f6eeffd942be2f7f8402ad68face
Jared Halpern <jared@byteMason.com>
2015-11-29 15:11:06 -0500

    first commit

--- SceneKit-Demo-2/ViewController.h
+++ SceneKit-Demo-2/ViewController.h
@@ -7,9 +7,17 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <SceneKit/SceneKit.h>
 
 @interface ViewController : UIViewController
 
+@property (nonatomic, strong) SCNView *sceneView;
+@property (nonatomic, strong) SCNNode *cameraNode;
+@property (nonatomic, strong) SCNNode *groundNode;
+@property (nonatomic, strong) SCNNode *lightNode;
+@property (nonatomic, strong) SCNNode *buttonNode;
+@property (nonatomic, strong) SCNNode *sphere1Node;
+@property (nonatomic, strong) SCNNode *sphere2Node;
 
 @end
 
