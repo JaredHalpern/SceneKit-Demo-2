0126050adfa4197c03e073c10331562e6f4ac52e
Jared Halpern <jared@byteMason.com>
2015-11-29 19:55:45 -0500

    WIP

--- SceneKit-Demo-2/ViewController.m
+++ SceneKit-Demo-2/ViewController.m
@@ -16,12 +16,79 @@
 
 - (void)viewDidLoad {
   [super viewDidLoad];
-  // Do any additional setup after loading the view, typically from a nib.
+
+  self.sceneView = [[SCNView alloc] initWithFrame:self.view.frame];
+  self.sceneView.scene = [SCNScene scene];
+  [self.view addSubview:self.sceneView];
+  
+  UITapGestureRecognizer *tapGestureRecognizer = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(sceneTapped:)];
+  tapGestureRecognizer.numberOfTapsRequired = 1;
+  tapGestureRecognizer.numberOfTouchesRequired = 1;
+  [self.sceneView addGestureRecognizer:tapGestureRecognizer];
+  
+  SCNFloor *groundGeometry = [SCNFloor floor];
+  groundGeometry.reflectivity = 0;
+  SCNMaterial *groundMaterial = [SCNMaterial material];
+  groundMaterial.diffuse.contents = [UIColor blueColor];
+  groundGeometry.materials = @[groundMaterial];
+  self.groundNode = [SCNNode nodeWithGeometry:groundGeometry];
+  
+  SCNCamera *camera = [SCNCamera camera];
+  camera.zFar = 10000;
+  
+  self.cameraNode = [SCNNode node];
+  self.cameraNode.camera = camera;
+  self.cameraNode.position = SCNVector3Make(-20, 15, 20);
+  
+  SCNLookAtConstraint *constraint = [SCNLookAtConstraint lookAtConstraintWithTarget:self.groundNode];
+  constraint.gimbalLockEnabled = YES;
+  self.cameraNode.constraints = @[constraint];
+  
+  SCNLight *ambientLight = [SCNLight light];
+  ambientLight.color = [UIColor darkGrayColor];
+  ambientLight.type = SCNLightTypeAmbient;
+  self.cameraNode.light = ambientLight;
+  
+  SCNLight *spotLight = [SCNLight light];
+  spotLight.type = SCNLightTypeSpot;
+  spotLight.castsShadow = YES;
+  spotLight.spotInnerAngle = 70.0;
+  spotLight.spotOuterAngle = 90.0;
+  spotLight.zFar = 500;
+  
+  self.lightNode = [SCNNode node];
+  self.lightNode.light = spotLight;
+  self.lightNode.position = SCNVector3Make(0, 25., 25.);
+  self.lightNode.constraints = @[constraint];
+  
+  SCNSphere *sphereGeometry = [SCNSphere sphereWithRadius:1.5];
+  SCNMaterial *sphereMaterial = [SCNMaterial material];
+  sphereMaterial.diffuse.contents = [UIColor greenColor];
+  sphereGeometry.materials = @[sphereMaterial];
+  self.sphere1Node = [SCNNode nodeWithGeometry:sphereGeometry];
+  self.sphere1Node.position = SCNVector3Make(-5, 1.5, 0);
+  self.sphere2Node = [SCNNode nodeWithGeometry:sphereGeometry];
+  self.sphere2Node.position = SCNVector3Make(5, 1.5, 0);
+
+  SCNBox *buttonGeometry = [SCNBox boxWithWidth:4 height:1 length:4 chamferRadius:0];
+  SCNMaterial *buttonMaterial = [SCNMaterial material];
+  buttonMaterial.diffuse.contents = [UIColor redColor];
+  buttonGeometry.materials = @[buttonMaterial];
+  self.buttonNode = [SCNNode nodeWithGeometry:buttonGeometry];
+  self.buttonNode.position = SCNVector3Make(-5, 0, 8);
+  
+  [self.sceneView.scene.rootNode addChildNode:self.cameraNode];
+  [self.sceneView.scene.rootNode addChildNode:self.groundNode];
+  [self.sceneView.scene.rootNode addChildNode:self.lightNode];
+  [self.sceneView.scene.rootNode addChildNode:self.buttonNode];
+  [self.sceneView.scene.rootNode addChildNode:self.sphere1Node];
+  [self.sceneView.scene.rootNode addChildNode:self.sphere2Node];
+  
 }
 
-- (void)didReceiveMemoryWarning {
-  [super didReceiveMemoryWarning];
-  // Dispose of any resources that can be recreated.
+- (void)sceneTapped:(UITapGestureRecognizer *)gesture
+{
+  
 }
 
 @end
